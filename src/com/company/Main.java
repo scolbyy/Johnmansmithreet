20092911b68a8e9dbb56a3d302c2becf82964aa8
scolbyy <shawncolby@billericak12.com>
2019-09-13 12:30:25 -0400

    commit her

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Goodbye");
+        //double x=Math.random();      // random number [0,1)
+        //int x=(int)(Math.random()*10);     //random double [0,10)
+        //int x=(int)(Math.random()*20);    //random int [0,20)
+        //int x=(int)(Math.random()+10);     //random int [10,20)
+        //double x=Math.random()*10;    // random double [0.0,10)
+        //int x=(int)(Math.random()*11);
+        //int x=(int)(Math.random()*6+1);
+        //int x=(int)(Math.random()*2);
+        //int x=(int)(Math.random()*6+1)+(int)(Math.random()*6+1);
+        //int x=(int)(Math.random()*36+65);
+        double x=Math.random()*9+1;
+        System.out.println(x);
     }
 }
---
+++ src/com/company/thesecondreet.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class thesecondreet {
+    public static void main(String[] args)
+    {
+        int a=method1();
+        double b=method2()
+        System.out.println(a);
+    }
+    public static int method1(){
+        int x=4;
+        int y=5;
+        int z=x*y;
+        return z;
+    }
+    public static double method2()
+    {
+        double x=3.5;
+        double y=8.2;
+        double z=x*y;
+        return z;
+
+
+    }
+}
