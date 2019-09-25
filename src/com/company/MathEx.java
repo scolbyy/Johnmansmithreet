f8cb6e18ea36fe46310e3efbf719b9797936f4f0
Shawn <shawncolby@billericak12.com>
2019-09-25 12:30:10 -0400

    dfeghdf

--- src/com/company/MathEx.java
+++ src/com/company/MathEx.java
@@ -1,5 +1,18 @@
 package com.company;
 
 public class MathEx {
-    private swap();
+    public static int swap (int x) {
+        int a = x % 10;
+        int b = x % 100 / 10;
+        int c = x / 100;
+        int y = c * 100 + b + a * 10;
+        return y;
+    }
+    public static void main (String [] args) {
+        int z = swap (172);
+        System.out.println(z);
+
 }
+
+    }
+
---
+++ src/com/company/timeleft.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class timeleft {
+    public static void main (String [] args) {
+        int h = timeLeft()
+
+    }
+
+    private static int timeLeft(int curHour, int curMin, int depHour, int depMin) {
+        int curHour
+    }
+
+
+
+    }
+}
+
